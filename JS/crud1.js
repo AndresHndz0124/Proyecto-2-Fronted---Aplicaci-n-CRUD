743045aed8d28ffca09e1a30f6b7d44e06d2e198
AndresHndz0124 <anfhernandezve@unal.edu.co>
2022-11-24 23:42:09 -0500

    Mejoras en codigo
    Se agregan funciones basicas
    Se agrega alerta para validar campo numerico

--- JS/crud1.js
+++ JS/crud1.js
@@ -10,6 +10,7 @@ else {
 	arrayUsuarios.map((e, j) => {
 		return $("#listaProductos").append(`<tr id="row${j}" class="Reg_A"><td><span class="mid">${j + 1} </span></td><td><span id="tittle${j}" class="rowData${j}">${e.Input}</span></td><td><span id="slry${j}" class="slryData${j}">${e.Salary} USD$</span></td><td><span id="desc${j}" class="descData${j}">${e.Descripcion}</span></td></span></td><td><button type="button" id="A${j}" class="btn btn-secondary" onclick="editar(${j},this)">Editar</button></td><td><button type="button" id="A${j}" class="btn btn-danger" onclick="eliminarRegistro(${j})">Eliminar</button></td></tr>`);
 	})
+	contador()
 }
 
 //Función para agregar registros
@@ -21,28 +22,39 @@ function add_row() {
 
 	//Esta parte valida si se ingreso la información en cada uno de los inputs y con base a eso calcula
 	//El tamaño del contenido almanecado hasta el momento
-	if (new_title && new_salary && new_description != "") {
-		var table = document.getElementById("listaProductos");
-		var table_len = (table.rows.length) - 1;
+	if (new_title && new_description != "") {
 
-		//Agrega la estructura del html con base a los registros
-		var row = table.insertRow(table_len + 1).outerHTML =
-			`<tr id="row${table_len - 1}" class="Reg_A"><td><span class="mid">${table_len} </span></td><td><span id="tittle${table_len - 1}" class="rowData${table_len - 1}">${new_title}</span></td><td><span id="slry${table_len - 1}" class="slryData${table_len - 1}">${new_salary} USD$</span></td><td><span id="desc${table_len - 1}" class="descData${table_len - 1}">${new_description}</span></td></span></td><td><button type="button" id="A${table_len - 1}" class="btn btn-secondary" onclick="editar(${table_len - 1},this)">Editar</button></td><td><button type="button" id="A${table_len - 1}" class="btn btn-danger" onclick="eliminarRegistro(${table_len - 1})">Eliminar</button></td></tr>`
+		if (new_salary > 0) {
+			var table = document.getElementById("listaProductos");
+			var table_len = (table.rows.length) - 1;
 
-		//Almacena los datos en el localStorage
-		let usuariosGuardados = JSON.parse(localStorage.getItem('usuarios'))
-		usuariosGuardados.push({ Input: new_title, Salary: new_salary, Descripcion: new_description })
-		localStorage.setItem('usuarios', JSON.stringify(usuariosGuardados))
-		//Vacia los inputs en la pagina
-		document.getElementById("Rol").value = "";
-		document.getElementById("Salario").value = "";
-		document.getElementById("descr").value = "";
+			//Agrega la estructura del html con base a los registros
+			var row = table.insertRow(table_len + 1).outerHTML =
+				`<tr id="row${table_len - 1}" class="Reg_A"><td><span class="mid">${table_len} </span></td><td><span id="tittle${table_len - 1}" class="rowData${table_len - 1}">${new_title}</span></td><td><span id="slry${table_len - 1}" class="slryData${table_len - 1}">${new_salary} USD$</span></td><td><span id="desc${table_len - 1}" class="descData${table_len - 1}">${new_description}</span></td></span></td><td><button type="button" id="A${table_len - 1}" class="btn btn-secondary" onclick="editar(${table_len - 1},this)">Editar</button></td><td><button type="button" id="A${table_len - 1}" class="btn btn-danger" onclick="eliminarRegistro(${table_len - 1})">Eliminar</button></td></tr>`
+
+			//Almacena los datos en el localStorage
+			let usuariosGuardados = JSON.parse(localStorage.getItem('usuarios'))
+			usuariosGuardados.push({ Input: new_title, Salary: new_salary, Descripcion: new_description })
+			localStorage.setItem('usuarios', JSON.stringify(usuariosGuardados))
+
+			//Vacia los inputs en la pagina
+			inputs_empty()
+		}
+
+		else {
+			alert('Debe ingresar un valor numerico o mayor a 0 en el campo "SALARIO" ');
+			return false;
+		}
 	}
 	//En caso de no tener datos arroja la alerta
 	else {
-		alert('Debe ingresar un valor');
-		return false;
-	}
+			alert('Debe ingresar un valor en todos los campos');
+			return false;
+		}
+	
+
+
+	contador()
 }
 
 
@@ -52,6 +64,7 @@ function Borrar() {
 		return
 	localStorage.removeItem('usuarios')
 	window.location.reload()
+	inputs_empty()
 }
 
 //Función para eliminar registros puntuales selecionados
@@ -66,6 +79,9 @@ function eliminarRegistro(no) {
 	usuariosGuardados.splice(no, 1)
 	localStorage.setItem('usuarios', JSON.stringify(usuariosGuardados))
 	cargar()
+	contador()
+	habilitar_botones()
+	inputs_empty()
 }
 
 //Se decide eliminar la estructura del HTML para poder recalcula la columna ID que determina la posición
@@ -87,7 +103,7 @@ function editar(no, ref) {
 	//Cambia la clase del boton para poder cambiar el color con CSS
 	ref.className = "btn btn-warning";
 
-	//Remueva el aributo para evitar mapear el evento del botón	
+	//Remueve el aributo para evitar mapear el evento del botón	
 	ref.removeAttribute("onclick");
 
 	//Mapea la estructua del HTML con base al id y el parametro "no" que determina la posición sobre la cual se hizo click
@@ -106,14 +122,21 @@ function editar(no, ref) {
 	document.getElementById("Salario").value = salry_data;
 	document.getElementById("descr").value = description_data;
 
+	//Función para ocultar el botón Agregar y eliminar todo
+	Ocultar_botones()
+
+	//LLama la función de editar y guardar cambios
 	ref.setAttribute("onclick", `saveEdit(${no},this)`);
 
-	//Oculta el botón Agregar y eliminar todo
-	document.getElementById('crear').style.display = 'none';
-	document.getElementById('borrarTodo').style.display = 'none';
+	//Actualiza el contador
+	contador()
+
+	// Vuelve habilitar los botones
+	habilitar_botones()
 }
 
 function saveEdit(no, ref) {
+
 	var new_rol = document.getElementById("Rol").value
 	var new_Salario = document.getElementById("Salario").value;
 	var new_description = document.getElementById("descr").value;
@@ -139,18 +162,50 @@ function saveEdit(no, ref) {
 	ref.setAttribute("onclick", `editar(${no},this)`);
 
 	//Devuelve los input a vacio
-	document.getElementById("Rol").value = "";
-	document.getElementById("Salario").value = "";
-	document.getElementById("descr").value = "";
+	inputs_empty()
 
 	//Develve el poton Crear nuevamente
-	document.getElementById('crear').style.display = 'block';
-	document.getElementById('borrarTodo').style.display = 'block';
+	habilitar_botones()
+	// document.getElementById('crear').style.display = 'block';
+	// document.getElementById('borrarTodo').style.display = 'block';
 
 }
 
+//Función para contar los registros y mostrarlo siempre en la pagina
+function contador() {
+	var table = document.getElementById("listaProductos");
+	var conteo = (table.rows.length) - 2;
+	var Contador_html = document.getElementById('counter');
+	var name = 'Registro';
+	if (conteo) {
+		if (conteo > 1) {
+			name = 'Registros';
+		}
+		Contador_html.innerHTML = conteo + ' ' + name
+	} else {
+		Contador_html.innerHTML = 0 + name;
+	}
+}
+
+function Ocultar_botones() {
+	document.getElementById('crear').style.display = 'none';
+	document.getElementById('borrarTodo').style.display = 'none';
+}
+
+function habilitar_botones() {
+	document.getElementById('crear').style.display = 'block';
+	document.getElementById('borrarTodo').style.display = 'block';
+}
+
+function inputs_empty() {
+	document.getElementById("Rol").value = "";
+	document.getElementById("Salario").value = "";
+	document.getElementById("descr").value = "";
+}
+
+
 
 //Agregar map para validar el guardar cambios que solo salga 1
-//Alimentar contador
+//Alimentar contador  ok
 //Alerta para validar campo numerico
 
